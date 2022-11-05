c935147620973cbc6bbf7209a170ef0b6f192dac
Yusuf Demir <yusufdemir8934@gmail.com>
2022-11-05 16:40:21 +0100

    05.11.22

---
+++ src/test/java/pojos/GoRestDataPojo.java
@@ -0,0 +1,88 @@
+package pojos;
+
+public class GoRestDataPojo {
+
+   /* {
+        "meta": null,
+            "data": {
+        "id": 2508,
+                "name": "Sharmila Deshpande VM",
+                "email": "deshpande_sharmila_vm@becker.name",
+                "gender": "female",
+                "status": "active"
+    }
+    }*/
+
+    // 1-) Tum keyler icin private variable'lar olusturuyoruz
+    private Integer id;
+    private String name;
+    private String email;
+    private String gender;
+    private String status;
+
+    // 2)Tüm parametrelerle ve parametresiz constroctorlar oluşturuyoruz.
+    public GoRestDataPojo(Integer id, String name, String email, String gender, String status) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+        this.status = status;
+    }
+
+    public GoRestDataPojo() {
+    }
+
+    //3) Public Getter ve Setter methodlarını oluşturuyoruz.
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    //4) toString() methodunu oluşturuyoruz.
+
+    @Override
+    public String toString() {
+        return "GoRestDataPojo{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", gender='" + gender + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/pojos/GoRestPojo.java
@@ -0,0 +1,38 @@
+package pojos;
+
+public class GoRestPojo {
+    private Object meta;
+    private GoRestDataPojo data;
+
+    public GoRestPojo(Object meta, GoRestDataPojo data) {
+        this.meta = meta;
+        this.data = data;
+    }
+
+    public GoRestPojo() {
+    }
+
+    public Object getMeta() {
+        return meta;
+    }
+
+    public void setMeta(Object meta) {
+        this.meta = meta;
+    }
+
+    public GoRestDataPojo getData() {
+        return data;
+    }
+
+    public void setData(GoRestDataPojo data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "GoRestPojo{" +
+                "meta=" + meta +
+                ", data=" + data +
+                '}';
+    }
+}
